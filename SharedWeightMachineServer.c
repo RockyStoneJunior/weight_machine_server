4173efeb0b2525182ddfecadd407edfac44f8a68
Rocky Stone <stone@511697856@qq.com>
2018-08-06 11:35:03 +0800

    add multithreading and complete the function that when recieve Headt response timestamp of the server

--- SharedWeightMachineServer.c
+++ SharedWeightMachineServer.c
@@ -8,12 +8,21 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+
 #define RECVBUFF_MAX_SIZE 1024
 #define PORT 9999
+
+#define RESPONSE_TIMESTAMP "Headt2@%d\n"
+
+void *connection_process_thread(void *);
 
 int main(int argc, char **argv)
 {
 	char recvbuff[RECVBUFF_MAX_SIZE];
+	pthread_t connection_process_thread_nr;
 
 	struct sockaddr_in servaddr;
 	
@@ -30,15 +39,35 @@ int main(int argc, char **argv)
 	{
 		int clientfd = accept(serverfd, (struct sockaddr *)&servaddr, &servaddr_len);
 		
-		int recv_len;
-		while((recv_len = read(clientfd, recvbuff, RECVBUFF_MAX_SIZE)) > 0)
-		{
-			write(STDOUT_FILENO, recvbuff, recv_len);
-			write(clientfd, recvbuff, recv_len);
-		}
-
-		close(clientfd);
+		pthread_create(&connection_process_thread_nr, NULL, connection_process_thread, &clientfd);
 	}
 
 	return 0;
+}
+
+void *connection_process_thread(void *clientfd_param)
+{
+	int clientfd = *(int *)clientfd_param;
+
+	char recvbuff[RECVBUFF_MAX_SIZE];
+	char sendbuff[128];	
+
+	int recv_len;
+	int send_len;
+
+	while((recv_len = read(clientfd, recvbuff, RECVBUFF_MAX_SIZE)) > 0)
+	{
+
+		if(!strncmp(recvbuff, "Headt", 5))
+		{
+			send_len = sprintf(sendbuff, RESPONSE_TIMESTAMP, (int)time(NULL));
+			write(clientfd, sendbuff, send_len);
+		}else{
+			write(clientfd, "Unknown Command\n", 16);
+		}
+	}
+
+	close(clientfd);
+
+	return NULL;
 }
