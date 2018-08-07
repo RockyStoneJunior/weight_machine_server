63fb049ab3381541f70facfe0911a142513f4f3e
Rocky Stone <stone@511697856@qq.com>
2018-08-07 15:40:17 +0800

    print thread id and current time in front of every information

--- SharedWeightMachineServer.c
+++ SharedWeightMachineServer.c
@@ -15,7 +15,9 @@
 #define RECVBUFF_MAX_SIZE 1024
 #define PORT 9999
 
-#define RESPONSE_TIMESTAMP "Headt2@%d\n"
+#define RESPONSE_TIMESTAMP "Heart@%d\n"
+
+#define WX_PLATFORM_LINK "Transmission Links@http://weixin.qq.com/q/02ta7UkQ4de921tsBFNr1g\n"
 
 void *connection_process_thread(void *);
 unsigned char calcFCS(unsigned char *pMsg, unsigned char len);
@@ -56,32 +58,33 @@ void *connection_process_thread(void *clientfd_param)
 	int recv_len;
 	int send_len;
 
+	pid_t tid = syscall(SYS_gettid);
+
 	while((recv_len = read(clientfd, recvbuff, RECVBUFF_MAX_SIZE)) > 0)
 	{
+		time_t current_time = time(NULL);
+		char *c_time_string = ctime(&current_time);
 
-		if(!strncmp(recvbuff, "Headt", 5))
+		if(!strncmp(recvbuff, "Heart", 5))
 		{
-			send_len = sprintf(sendbuff, RESPONSE_TIMESTAMP, (int)time(NULL));
+			send_len = sprintf(sendbuff, RESPONSE_TIMESTAMP, current_time);
 			write(clientfd, sendbuff, send_len);
+
+			printf("%s---TID:%d Heart---\n", c_time_string,tid);
 		}else{
-			printf("the data reveived is: ");	
-			
-			for(int i = 0; i < 11; i++)
-			{
-				printf("0x%x ", recvbuff[i]);
-			}
-
-			printf("\n");
-
 			unsigned char checksum = calcFCS(recvbuff, 10);
-			printf("the checksum is %x\n", checksum);
 		
 			if((recvbuff[0] == 0x5A) && (recvbuff[1] == 0x0B) && (recvbuff[10] == checksum))
 			{
-				printf("receive 0x5A 0x0B, the recvbuff[10] is %x, checksum is %x\n", recvbuff[10], checksum);
+				int wx_platform_link_len = strlen(WX_PLATFORM_LINK);	
+				write(clientfd, WX_PLATFORM_LINK, wx_platform_link_len);
+
+				printf("%s---TID:%d machineid[0x%x 0x%x 0x%x 0x%x 0x%x 0x%x] weight[0x%x 0x%x]---\n", c_time_string, tid, recvbuff[2], recvbuff[3], recvbuff[4], recvbuff[5], recvbuff[6], recvbuff[7], recvbuff[8], recvbuff[9]);
+			}else if(!strncmp(recvbuff, "Links Success", 13)){
+				printf("%s---TID:%d Links Success---\n", c_time_string, tid);
 			}else{
 				write(clientfd, "Unknown Command\n", 16);
-				break;
+				printf("%s---TID:%d Unknown Command---\n", c_time_string, tid);
 			}
 		}
 	}
